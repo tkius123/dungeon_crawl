d88b54a866edc6a185c9d28db000c116b6ff67d3
Tim Lee <artemis930101>
2023-10-09 12:45:11 -0500

    master hero selection done

--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -5,7 +5,21 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     Shell.cmd("cls")
     Shell.info("Start by choosing your hero:")
     heroes = DungeonCrawl.Heroes.all()
-    heroes |> Enum.map(&(&1.name)) |> display_options
+
+    find_hero_by_index = &Enum.at(heroes, &1)
+    get_data = &("You have chosen #{Map.get(&1, :name)}")
+
+    heroes
+    |> Enum.map(&(&1.name))
+    |> display_options
+    |> generate_questions
+    |> Shell.prompt
+    |> parse_answer
+    |> find_hero_by_index.()
+    |> confirm_hero
+    |> get_data.()
+    |> Shell.info()
+
   end
 
   def display_options(options) do
@@ -14,5 +28,22 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     |> Enum.each(fn {option, index} ->
       Shell.info("#{index} - #{option}")
     end)
+    options
+  end
+
+  defp generate_questions(options) do
+    options = Enum.join(1..Enum.count(options), ",")
+    "Which one? [#{options}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+
+  defp confirm_hero(chosen_hero) do
+    Shell.cmd("cls")
+    Shell.info(chosen_hero.description)
+    if Shell.yes?("Confirm?"), do: chosen_hero, else: start()
   end
 end
