34011417ac7eb7f16f87cecb6c40bd2c28c7bfdc
Tim Lee <artemis930101>
2023-10-10 18:10:05 -0500

    feature/try-catch implement try-rescue-catch

--- lib/dungeon_crawl/cli/base_command.ex
+++ lib/dungeon_crawl/cli/base_command.ex
@@ -34,21 +34,45 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     Shell.cmd("clear")
   end
 
-  def ask_for_option(options) do
-    index = ask_for_index(options)
-    chosen_option = Enum.at(options, index)
-    chosen_option
-      || (display_invalid_option() && ask_for_option(options))
-  end
-
   def generate_question(options) do
     options = Enum.join(1..Enum.count(options), ",")
     "Which one? [#{options}]\n"
   end
 
-  def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
+  def parse_answer!(answer) do
+    case Integer.parse(answer) do
+      :error ->
+        raise DungeonCrawl.CLI.InvalidOptionError
+      {option, _} ->
+        option - 1
+    end
+  end
+
+  def find_option_by_index!(index, options) do
+    Enum.at(options, index)
+      || raise DungeonCrawl.CLI.InvalidOptionError
+  end
+
+  def ask_for_option(options) do
+    try do
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt
+      |> parse_answer!
+      |> find_option_by_index!(options)
+    rescue
+      e in DungeonCrawl.CLI.InvalidOptionError ->
+        display_error(e)
+        ask_for_option(options)
+    end
+  end
+
+  def display_error(e) do
+    Shell.cmd("cls")
+    Shell.error(e.message)
+    Shell.prompt("Press Enter to continue.")
+    Shell.cmd("cls")
   end
 
   def confirm_choice(choice) do
