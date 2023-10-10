34a283fb26d6636dabfcfd7af9917730ad68da4d
Tim Lee <artemis930101>
2023-10-10 09:59:59 -0500

    isolate-side-effects changed room_actions_choices

--- lib/dungeon_crawl/cli/room_actions_choice.ex
+++ lib/dungeon_crawl/cli/room_actions_choice.ex
@@ -4,8 +4,6 @@ defmodule DungeonCrawl.CLI.RoomActionsChoice do
 
   def start(room) do
     room_actions = room.actions
-    find_action_by_index = &(Enum.at(room_actions, &1))
-
     Shell.info(room.description)
 
     chosen_action =
@@ -14,7 +12,7 @@ defmodule DungeonCrawl.CLI.RoomActionsChoice do
       |> generate_question
       |> Shell.prompt
       |> parse_answer
-      |> find_action_by_index.()
+      |> (&(Enum.at(room_actions, &1))).()
       |> show_choice
 
     {room, chosen_action}
