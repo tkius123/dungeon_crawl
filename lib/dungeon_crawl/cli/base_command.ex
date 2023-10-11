02417a9f2c25bc7b2952b33f0ad9028516911bee
Tim Lee <artemis930101>
2023-10-11 04:39:45 -0500

    feature/with implement with-else

--- lib/dungeon_crawl/cli/base_command.ex
+++ lib/dungeon_crawl/cli/base_command.ex
@@ -35,15 +35,31 @@ defmodule DungeonCrawl.CLI.BaseCommands do
   end
 
   def ask_for_option(options) do
-    index = ask_for_index(options)
-    chosen_option = Enum.at(options, index)
-    if chosen_option do
-      chosen_option
-    else
-      display_invalid_option()
-      ask_for_option(options)
-    end
+    answer =
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt
 
+    with {option, _} <- Integer.parse(answer),
+      chosen when chosen != nil <- Enum.at(options, option - 1) do
+        chosen
+    else
+      :error -> retry(options)
+      nil -> retry(options)
+    end
+  end
+
+  def retry(options) do
+    display_error("Invalid option")
+    ask_for_option(options)
+  end
+
+  def display_error(message) do
+    Shell.cmd("clear")
+    Shell.error(message)
+    Shell.prompt("Press Enter to continue.")
+    Shell.cmd("clear")
   end
 
   def generate_question(options) do
